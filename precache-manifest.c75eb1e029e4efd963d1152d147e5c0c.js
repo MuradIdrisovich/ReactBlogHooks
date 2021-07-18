757bacf45f17fbf085313507c367ab863d8e8829
Murad <2345murad@gmail.com>
2021-07-18 20:18:24 +0300

    Updates

---
+++ precache-manifest.c75eb1e029e4efd963d1152d147e5c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd52d81dc84d5e613fe30511f08d97d",
+    "url": "/ReactBlogHooks/index.html"
+  },
+  {
+    "revision": "50d8bd84ae7fe6b015e6",
+    "url": "/ReactBlogHooks/static/css/main.2668b83c.chunk.css"
+  },
+  {
+    "revision": "243e8dd6093da24d24ff",
+    "url": "/ReactBlogHooks/static/js/2.008b20e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactBlogHooks/static/js/2.008b20e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d8bd84ae7fe6b015e6",
+    "url": "/ReactBlogHooks/static/js/main.57561db5.chunk.js"
+  },
+  {
+    "revision": "1e138ef04837b6fcfaa4",
+    "url": "/ReactBlogHooks/static/js/runtime-main.2ad3799d.js"
+  }
+]);
